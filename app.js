4274c4018f41c3e33707da6930309e1503320972
UrtziFM <urtzi.arana13@gmail.com>
2020-10-26 20:37:41 +0100

    starting with app.js script

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const grid = document.querySelector('.grid')
+    const width = 10
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-pause')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
 
+    console.log(squares)
     
 })
